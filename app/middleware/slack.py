0523a03201125467bb1d3921667b9fa64ec8f467
wata-tsugarian <wata.tsugarian@gmail.com>
2025-01-26 16:28:31 +0900

    chから投稿取得処理

--- app/middleware/slack.py
+++ app/middleware/slack.py
@@ -1,6 +1,6 @@
+import datetime
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-
 from settings import settings
 
 
@@ -10,12 +10,48 @@ class Slack:
     def __init__(self):
         self.client = WebClient(token=settings.slack_bot_token)
 
-    def post_message(self):
-        text = "テスト投稿"
+    def _gen_unix_time(self) -> float:
+        now = datetime.datetime.now()
+        six_days_ago = now - datetime.timedelta(days=6)
+        return six_days_ago.timestamp()
+
+    def _gen_post_message(self, win_count: int) -> str:
+        now = datetime.datetime.now()
+        now_date = now.strftime("%Y/%m/%d")
+        six_days_ago = now - datetime.timedelta(days=6)
+        six_days_ago_date = six_days_ago.strftime("%Y/%m/%d")
+
+        return f"""
+今週もお疲れ様でし！
+{six_days_ago_date}から{now_date}のwin数は{win_count}件でした！
+18:00からのwin-session盛り上がっていきましょう！
+
+今日の本投稿後以降の駆け込みwinは除きますmm
+"""
+
+    def fetch_post_history(self) -> list[str]:
+        unix_6days_ago = self._gen_unix_time()
+
+        try:
+            response = self.client.conversations_history(
+                oldest=unix_6days_ago,
+                channel=settings.slack_channel_id,
+                limit=100,
+            )
+            return response["messages"]
+
+        except SlackApiError as e:
+            print(f"Error fetching history: {e}")
+
+    def post_message(self, post_history: list[str]):
+        win_count = len(post_history)
+
+        message = self._gen_post_message(win_count=win_count)
+
         try:
             self.client.chat_postMessage(
                 channel=settings.slack_channel_id,
-                text=text,
+                text=message,
             )
 
         except SlackApiError as e:
