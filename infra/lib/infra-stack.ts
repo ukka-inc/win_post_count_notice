7d6dd0fe5a5a23678978d66bb4720305ff860ab2
Hiroyuki OGASAWARA <ogasawara.h@ukka.green>
2025-02-05 11:30:13 +0900

    cdk構築

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,7 +3,8 @@ import { Construct } from "constructs";
 import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaPython from "@aws-cdk/aws-lambda-python-alpha";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,18 +17,18 @@ export class InfraStack extends cdk.Stack {
     const serviceName = "win-post-count-notice";
 
     // Slack Channel ID
-    let slackChaanelId = "";
+    let slackChanelId = "";
     switch (ENV) {
       case "dev":
-        slackChaanelId = "C073A3UBU83";
+        slackChanelId = "C073A3UBU83";
         break;
       case "prd":
-        slackChaanelId = "C03RJMDNVP0";
+        slackChanelId = "C03RJMDNVP0";
         break;
     }
 
     // Secrets Manager
-    const secretName = `${ENV}/${serviceName}`;
+    const secretName = `${serviceName}/${ENV}`;
     const secretSlackBotToken = secretsmanager.Secret.fromSecretNameV2(
       this,
       "SLACK_BOT_TOKEN",
@@ -35,20 +36,38 @@ export class InfraStack extends cdk.Stack {
     );
 
     // Lambda
-    new lambdaPython.PythonFunction(this, `${serviceName}-${ENV}-lambda`, {
-      functionName: `${ENV}-${serviceName}`,
-      runtime: lambda.Runtime.PYTHON_3_12,
-      entry: "../app/",
-      index: "app_lambda.py",
-      handler: "handler",
-      memorySize: 256,
-      timeout: cdk.Duration.minutes(15),
-      environment: {
-        ENV: `${ENV}`,
-        SLACK_BOT_TOKEN: `${secretSlackBotToken
-          .secretValueFromJson("SLACK_BOT_TOKEN")
-          .unsafeUnwrap()}`,
-      },
+    const winPostCountNoticeLambda = new lambdaPython.PythonFunction(
+      this,
+      `${serviceName}-${ENV}-lambda`,
+      {
+        functionName: `${ENV}-${serviceName}`,
+        runtime: lambda.Runtime.PYTHON_3_12,
+        entry: "../app/",
+        index: "app_lambda.py",
+        handler: "lambda_handler",
+        memorySize: 256,
+        timeout: cdk.Duration.minutes(15),
+        environment: {
+          ENV: `${ENV}`,
+          SLACK_BOT_TOKEN: `${secretSlackBotToken
+            .secretValueFromJson("SLACK_BOT_TOKEN")
+            .unsafeUnwrap()}`,
+          SLACK_CHANNEL_ID: `${slackChanelId}`,
+        },
+      }
+    );
+
+    // Scheduler
+    const rule = new events.Rule(this, `${serviceName}-${ENV}-rule`, {
+      ruleName: `${serviceName}-${ENV}-rule`,
+      schedule: events.Schedule.cron({
+        minute: "55",
+        hour: "9",
+        weekDay: "FRI",
+      }),
     });
+
+    // Add Target
+    rule.addTarget(new targets.LambdaFunction(winPostCountNoticeLambda));
   }
 }
